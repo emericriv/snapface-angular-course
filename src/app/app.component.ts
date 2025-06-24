5571df96756cd8b9b4d6c0235beb7ee9e246afac
emericriv <e19riviere@gmail.com>
2025-06-24 07:17:06 +0200

    first commit, to begin the course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FaceSnapComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'snapface-angular-course';
-}
+export class AppComponent {}
