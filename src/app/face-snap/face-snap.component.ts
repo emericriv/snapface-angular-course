2b8621326a758cc1b61110879751024af55bc1b0
emericriv <e19riviere@gmail.com>
2025-06-24 07:38:55 +0200

    Display data commit

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   imports: [],
   templateUrl: './face-snap.component.html',
-  styleUrl: './face-snap.component.scss'
+  styleUrl: './face-snap.component.scss',
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  snaps!: number;
+  imageUrl!: string;
 
+  ngOnInit(): void {
+    this.title = 'Goku';
+    this.description = 'A simple face snap component';
+    this.createdAt = new Date();
+    this.snaps = 0;
+    this.imageUrl =
+      'https://gallerymurciano.com/wp-content/uploads/2024/03/Young-Goku.jpg';
+  }
 }
